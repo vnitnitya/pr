b6a77f7e19225e75860cfb7624d2426e406a5916
vnitnitya <nityakumari93@gmail.com>
2015-03-03 09:25:34 +0530

    [COMMIT_MESSAGE

---
+++ pr/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var mongojs = require('mongojs');
+var db= mongojs("cse",["serviceClients"]);
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+app.get("/serviceClients",function(req,res){
+db.serviceClients.find(function(err,docs){
+		res.json(docs);
+	});
+});
+app.post("/serviceClients",function(req,res){
+  var svc = req.body;
+  console.log(svc);
+  db.serviceClients.insert(req.body, function(err,doc){
+  res.json(doc);
+});
+});
+app.get("/serviceClients/:id",function(req,res){
+  var id= req.params.id;
+  console.log(id);
+  db.serviceClients.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+  	res.json(doc);
+  	});
+});
+
+app.delete("/serviceClients/:id",function(req,res){
+var id = req.params.id;
+console.log(id);
+db.serviceClients.remove({_id:mongojs.ObjectId(id)},function(err,doc){
+	res.json(doc);
+});
+});
+app.listen(3000);
